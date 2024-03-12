6c029226af43149ef07e01fe92bb6d2d60be8957
Luis Lopez <Luisloppez01@outlook.com>
2024-03-12 15:07:39 -0600

    update controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+use Illuminate\Http\JsonResponse;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $response = ["status"=>0,"msg"=>"","data"=>""];
+        $users = User::all();
+        $data = new UserCollection($users);
+        $response["status"]=200;
+        $response["msg"]="Operacion completada con éxito";
+        $response["data"]=$data;
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $response = ["status"=>0,"msg"=>""];
+        $data = $request->all();
+        $data['password'] = Hash::make($request->input('password'));
+        new UserResource(User::create($data));
+        $response["status"]=200;
+        $response["msg"]="Operacion completada con éxito";
+        return response()->json($response);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $response = ["status"=>0,"msg"=>"","data"=>""];
+        try {
+            $user = User::findOrFail($id);
+            $data = new UserResource($user);
+            $response["status"]=200;
+            $response["msg"]="Operacion completada con éxito";
+            $response["data"]=$data;
+            return response()->json($response);
+        }catch (ModelNotFoundException $e){
+            $response["status"]=404;
+            $response["msg"]="Usuario no encontrado";
+            $response["data"]=null;
+            return response()->json($response);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $response = ["status"=>0,"msg"=>""];
+        try {
+            $user = User::findOrFail($id);
+            $user->update($request->all());
+            $response["status"]=200;
+            $response["msg"]="Operacion completada con éxito";
+            return response()->json($response);
+        }catch (ModelNotFoundException $e){
+            $response["status"]=404;
+            $response["msg"]="Usuario no encontrado";
+            return response()->json($response);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $response = ["status"=>0,"msg"=>""];
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            $response["status"]=200;
+            $response["msg"]="Operacion completada con éxito";
+            return response()->json($response);
+        }catch (ModelNotFoundException $e){
+            $response["status"]=404;
+            $response["msg"]="Usuario no encontrado";
+            return response()->json($response);
+        }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,4 +16,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([], function (){
+    Route::get('', [\App\Http\Controllers\Menu\HomeController::class, 'index'])->name('home');
+    Route::apiResource('users',\App\Http\Controllers\UserController::class);
+    Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
+    Route::apiResource('locations',\App\Http\Controllers\LocationController::class);
+    Route::apiResource('chains',\App\Http\Controllers\ChainController::class);
+    Route::apiResource('hauliers',\App\Http\Controllers\HaulierController::class);
+    Route::apiResource('entities',\App\Http\Controllers\EntityController::class);
+    Route::apiResource('verifications',\App\Http\Controllers\VerificationController::class);
+    Route::apiResource('units',\App\Http\Controllers\UnitController::class);
+    Route::apiResource('vehicles',\App\Http\Controllers\VehicleController::class);
+    Route::apiResource('types',\App\Http\Controllers\TypeController::class);
+    Route::apiResource('components',\App\Http\Controllers\ComponentController::class);
+    Route::apiResource('fields',\App\Http\Controllers\FieldController::class);
+    Route::apiResource('mailing_lists',\App\Http\Controllers\MailingListController::class);
+    Route::apiResource('update_units',\App\Http\Controllers\UpdateUnitController::class);
+    Route::apiResource('detail_values',\App\Http\Controllers\DetailValueController::class);
+    Route::apiResource('catalog_special_features',\App\Http\Controllers\CatalogSpecialFeatureController::class);
+    Route::apiResource('axis_catalogs',\App\Http\Controllers\AxisCatalogController::class);
+    Route::apiResource('car_brands',\App\Http\Controllers\CarBrandController::class);
+    Route::apiResource('supporting_features',\App\Http\Controllers\SupportingFeatureController::class);
+    Route::apiResource('expired_verifications',\App\Http\Controllers\ExpiredVerificationController::class);
 });
